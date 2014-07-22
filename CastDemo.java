23d7ad63741e6fca22b418c19c2f3e168a53d456
jayarbautista <j.bautista@klab.com>
2014-07-22 11:27:11 +0800

    Demo of casting data types

---
+++ CastDemo.java
@@ -0,0 +1,28 @@
+//Casting Demonstration
+class CastDemo {
+	public static void main(String args[])
+	{
+		double x ,y;
+		byte b;
+		int i;
+		char ch;
+		
+		x = 24.0;
+		y = 5.0;
+		
+		i = (int)(x / y); //cast double to int
+		System.out.println("Integer outcome of x / y: " + i);
+		
+		i = 100;
+		b = (byte) i;
+		System.out.println("Value of b: " + b);
+		
+		i = 257;
+		b = (byte) i;
+		System.out.println("Value of b: " + b);
+		
+		b = 88;
+		ch = (char) b;
+		System.out.println("ch: " + ch);
+	}
+}
