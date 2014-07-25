8e54b2832fd6ccef3d9d2827e1e4c8b7f865643f
jayarbautista <j.bautista@klab.com>
2014-07-25 14:41:36 +0800

    Demo for generic constructor

---
+++ GenConsDemo.java
@@ -0,0 +1,23 @@
+//Generic constructor
+class Summation {
+	private int sum;
+	
+	<T extends Number> Summation(T arg) {
+		sum = 0;
+		
+		for( int i = 0; i <= arg.intValue(); i++ )
+			sum += i;
+	}
+	
+	int getSum() {
+		return sum;
+	}
+}
+
+class GenConsDemo {
+	public static void main(String args[]) {
+		Summation ob = new Summation(5.0);
+		
+		System.out.println("Summation of 24.0 is " + ob.getSum()); //15
+	}
+}
