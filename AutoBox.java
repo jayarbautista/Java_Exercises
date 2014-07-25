2a9e6b4e1a88fcd470b66a00d7fd0d46b6a676f6
jayarbautista <j.bautista@klab.com>
2014-07-25 09:07:02 +0800

    Demo for autoboxing and auto-unboxing

---
+++ AutoBox.java
@@ -0,0 +1,8 @@
+//Demo for autoboxing and auto-unboxing
+class AutoBox {
+	public static void main(String args[]) {
+		Integer iOb = 100; //autobox
+		int i = iOb; //auto-unbox
+		System.out.println(i + " " + iOb);
+	}
+}
