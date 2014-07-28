e40f35a21178d354d9b9427a2268aa02535bacc5
jayarbautista <j.bautista@klab.com>
2014-07-28 10:50:42 +0800

    Demo for mouse event handlers

---
+++ MouseEvents.java
@@ -0,0 +1,72 @@
+//Demo for mouse event handlers
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+/*
+<applet code="MouseEvents" height=100 width=300>
+*</applet>
+*/
+
+public class MouseEvents extends Applet implements MouseListener, MouseMotionListener {
+	String msg = "";
+	int mouseX = 0;
+	int mouseY = 0;
+	
+	public void init() {
+		addMouseListener(this);
+		addMouseMotionListener(this);
+	}
+	
+	//Handle mouse clicked
+	public void mouseClicked(MouseEvent me) {
+		mouseX = 0;
+		mouseY = 10;
+		msg = "Mouse Clicked";
+		repaint();
+	}
+	
+	public void mouseEntered(MouseEvent me) {
+		mouseX = 0;
+		mouseY = 10;
+		msg = "Mouse Entered";
+		repaint();
+	}
+	
+	public void mouseExited(MouseEvent me) {
+		mouseX = 0;
+		mouseY = 10;
+		msg = "Mouse Exited";
+		repaint();
+	}
+	
+	public void mousePressed(MouseEvent me) {
+		mouseX = me.getX();
+		mouseY = me.getY();
+		msg = "Down";
+		repaint();
+	}
+	
+	public void mouseReleased(MouseEvent me) {
+		mouseX = me.getX();
+		mouseY = me.getY();
+		msg = "Up";
+		repaint();
+	}
+	
+	public void mouseDragged(MouseEvent me) {
+		mouseX = me.getX();
+		mouseY = me.getY();
+		msg = "*";
+		showStatus("Dragging mouse at " + mouseX + ", " + mouseY);
+		repaint();
+	}
+	
+	public void mouseMoved(MouseEvent me) {
+		showStatus("Moving mouse at " + me.getX() + ", " + me.getY());
+	}
+	
+	public void paint(Graphics g) {
+		g.drawString(msg,mouseX,mouseY);
+	}
+}
