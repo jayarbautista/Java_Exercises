3504349c4f8269dad06255a2612c779b59aad544
jayarbautista <j.bautista@klab.com>
2014-07-28 10:48:59 +0800

    The applet skeleton

---
+++ AppletSkel.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="AppletSkel" width=200 height=60>
+</applet>
+*/
+
+public class AppletSkel extends Applet {
+	
+	public void init() {
+		//initialization
+	}
+	
+	public void start() {
+		//start or resume execution
+	}
+	
+	public void stop() {
+		//suspends execution
+	}
+	
+	public void destroy() {
+		//perform shutdown activities
+	}
+	
+	public void paint(Graphics g) {
+		//redisplay contents of window
+	}
+}
